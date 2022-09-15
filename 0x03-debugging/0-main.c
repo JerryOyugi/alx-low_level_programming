4981bc2a1e38bf0fb00f5d87c529712c1c145c16
Jerry Oyugi <jerryomondi9@gmail.com>
2022-09-15 16:03:29 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - test if function gives an output correctly when given a case 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
