2529e079e925533691bcaab53a9881c5eefd593a
Jerry Oyugi <jerryomondi9@gmail.com>
2022-09-19 14:09:48 -0700

    Copies one string to another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies one string to another
+ * @src: The string to be copied
+ * @dest: The string to be copied to
+ * Return: pointer to @dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *string = dest;
+
+	for (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (string);
+}
