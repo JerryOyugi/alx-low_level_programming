a4fdfaef62f68dd50bde4f6a52d6f0d69e415349
Jerry Oyugi <jerryomondi9@gmail.com>
2022-09-28 15:24:27 -0700

    if a number is a prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,11 +9,11 @@
  */
 int is_prime_number(int n)
 {
-	int i;
+	int n;
 
-	if (i % 1 == 0 && i % i == 0)
+	if (n % 1 == 0 && n % i == 0)
 		return (1);
 	else
 		return (0);
-	return (is_prime number(n + 1));
+	return (is_prime_number(n + 1));
 }
