0bbf76d1d77c1af4bb89f185cb3b685a94b24769
Jerry Oyugi <jerryomondi9@gmail.com>
2022-10-03 07:33:15 -0700

    Multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: argument count
+ * @argv: arguments as string array
+ *
+ * description: Multiplies two numbers
+ * Return: 0 (success), 1 otherwise
+ */
+int main(int argc, char **argv)
+{
+	int x, y;
+
+	if (arg < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x = atoi(agrv[1]);
+	y = atoi(agrv[2]);
+	printf("%d\n", x * y);
+	return (0);
+}
