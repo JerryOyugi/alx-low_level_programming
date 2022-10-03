5fb098e6ed22da208c30e504558c94e34bd665a2
Jerry Oyugi <jerryomondi9@gmail.com>
2022-10-03 07:55:06 -0700

    adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: arguments as string arrays
+ *
+ * description: adds positive numbers
+ *
+ * Return: 0 (success), 1 if symbol is used instead of a digit
+ */
+int main(int argc, char **argv)
+{
+	int i, n, sum = 0;
+	char *ptr;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; argv[i]; i++)
+	{
+		n = strol(argv[i], &ptr, 10);
+		if (*ptr)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
