04aeb6841fd6bb8967fa410591e53784aea98ddc
Jerry Oyugi <jerryomondi9@gmail.com>
2022-10-03 06:16:54 -0700

    Prints the program name followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -11,8 +11,10 @@
  *
  * Return: 0 (success)
  */
-int main(int __attribute__((unused)) argc, char *argv[])
+int main(int argc, char **argv)
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
