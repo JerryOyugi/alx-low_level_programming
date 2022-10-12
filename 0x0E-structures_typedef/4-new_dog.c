915636207fd6f42f6d8cef9205109a82dd56a890
Jerry Oyugi <jerryomondi9@gmail.com>
2022-10-12 16:53:52 -0700

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,7 +10,7 @@
  *
  * Return: pointer to a new dog
  */
-dog_t *new_dog(char *name, foat age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int nl, ol, i;
 	dog_t *dog;
