0a625497cd7e9b8b7e3e7d08a7fdc541a1ed7dc1
Jerry Oyugi <jerryomondi9@gmail.com>
2022-10-12 15:15:15 -0700

    Prints the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the nme of the file
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
