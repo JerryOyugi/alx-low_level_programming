d8dd873e2d3f7222454082ab3ff096bc481dc6d7
Jerry Oyugi <jerryomondi9@gmail.com>
2022-10-24 16:01:53 -0700

    Prints string of words before the meain function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_first -a function to be executed
+ *
+ */
+void __attribute__ ((__constructor__)) print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
